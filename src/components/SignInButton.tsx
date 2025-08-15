4a7f2f7c91ea3655dea4d1966e76ed80c07fff3e
bowale-os <danbowale@gmail.com>
2025-08-15 03:54:06 -0500

    added redirect url to google sign in

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -34,7 +34,7 @@ export function SignInWithGoogle() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.location.origin, 
+        redirectTo: 'https://forum-production-659a.up.railway.app/', 
       },
     });
 
